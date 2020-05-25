e1706b757d3a1017407a4c79ae385461647feec6
Philipp Melab <philipp.melab@gmail.com>
2020-05-25 15:39:23 +0200

    Add first story for project listing

---
+++ stories/2-Projects.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProjectsPageLayout from "../src/pages/projects"
+
+export default {
+  title: 'Projects',
+  component: ProjectsPageLayout,
+};
+
+export const Filled = () => (
+  <ProjectsPageLayout/>
+);
