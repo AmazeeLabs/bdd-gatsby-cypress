e67e8ec24e0d4f942a8db3b3190d2258d48305e3
Philipp Melab <philipp.melab@gmail.com>
2020-05-26 08:01:51 +0200

    Stub metadata hook in projects story.

--- stories/2-Projects.stories.js
+++ stories/2-Projects.stories.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react"
 import ProjectsPageLayout from "../src/pages/projects"
+import * as metadata from "../src/hooks/metadata"
+import { stub } from "sinon"
 
 export default {
-  title: 'Projects',
+  title: "Projects",
   component: ProjectsPageLayout,
-};
+}
 
-export const Filled = () => (
-  <ProjectsPageLayout/>
-);
+export const Filled = () => {
+  stub(metadata, "useSiteMetadata").returns({
+    site: {
+      siteMetadata: {
+        title: "Project Management",
+      },
+    },
+  })
+  return <ProjectsPageLayout />
+}
