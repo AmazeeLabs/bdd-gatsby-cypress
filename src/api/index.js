b2d8a201cd8a73cfbdd9a1883a1f482f1b5343c9
Philipp Melab <philipp.melab@gmail.com>
2020-08-26 12:02:11 +0200

    Added GraphQL mutation for adding a project.

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server');
-const {projects} = require('../../cypress/fixtures/examples');
+const moment = require("moment")
+const { ApolloServer, gql } = require("apollo-server")
+const { projects } = require("../../cypress/fixtures/examples")
 
 const typeDefs = gql`
   type Project {
@@ -12,16 +13,45 @@ const typeDefs = gql`
   type Query {
     projects: [Project!]!
   }
-`;
+
+  input ProjectInput {
+    name: String!
+    startDate: String
+    endDate: String
+  }
+
+  type Mutation {
+    addProject(input: ProjectInput): Project!
+  }
+`
 
 const resolvers = {
   Query: {
     projects: () => projects,
   },
-};
 
-const server = new ApolloServer({ typeDefs, resolvers });
+  Mutation: {
+    addProject: (_, { input }) => {
+      const project = {
+        id: projects.length + 1,
+        name: input.name,
+        startDate: input.startDate
+          ? moment(input.startDate).format()
+          : moment().format(),
+        endDate: input.endDate
+          ? moment(input.endDate).format()
+          : moment()
+              .add(2, "weeks")
+              .format(),
+      }
+      projects.push(project)
+      return project
+    },
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})
