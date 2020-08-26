690e115e5238622580508af7c960330f5944907d
Philipp Melab <philipp.melab@gmail.com>
2020-08-26 11:26:02 +0200

    Streamlined test execution and coverage reporting.

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: ".nyc_output",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -29,12 +29,7 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ["json"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
